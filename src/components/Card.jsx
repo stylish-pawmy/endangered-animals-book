5d219990638be325e15ae64c2b2ec285de21e940
stylish-pawmy <ambesi2026@gmail.com>
2023-09-10 12:54:50 +0100

    Style animal card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,19 @@
 import "../App.css";
 import { default as TigerImg } from "../assets/white-tiger.jpg";
+import { default as GeoIndex } from "../assets/geo-index.svg";
 
 export default function Card()
 {
+    
     return (
         <article className="card">
             <img src={TigerImg}/>
             <div className="card--body">
-                <p className="card--location">India <a>View On Google Maps</a></p>
-                <h3>The White Tiger</h3>
+                <p className="card--location">
+                    <img src={GeoIndex} /> India
+                    <a href="https://goo.gl/maps/wDkYMztGXFNhJyao6">View On Google Maps</a>
+                    </p>
+                <h3 className="card--title">The White Tiger</h3>
                 <p className="card--alt-name">Ghost Tiger, Royal White Tiger</p>
                 <p className="card--description">
                     The white tiger, scientifically known as Panthera tigris,
