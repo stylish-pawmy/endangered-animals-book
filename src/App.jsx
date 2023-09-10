0f431083dd369c0c1c7073c849bd4fcc978c3fca
stylish-pawmy <ambesi2026@gmail.com>
2023-09-10 13:54:39 +0100

    Populate animal database and use in components

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,27 @@ import { default as TopBar } from "./components/TopBar.jsx";
 import { default as Card } from "./components/Card.jsx";
 import './App.css'
 
+//Data
+import { default as AnimalData } from "./data/animalData.js";
+
 function App() {
+  const animalCards = AnimalData.map((animal) =>
+      <Card
+          key={animal.key}
+          img={animal.img}
+          location={animal.location}
+          locationLink={animal.locationLink}
+          title={animal.title}
+          altNames={animal.altNames}
+          description={animal.description}
+      />
+  );
+
   return (
     <>
       <TopBar />
       <main>
-        <Card
-          key={0}
-          img="/public/white-tiger.jpg"
-          location="India"
-          locationLink="https://goo.gl/maps/wDkYMztGXFNhJyao6"
-          title="The White Tiger"
-          altNames="Ghost Tiger, Royal White Tiger"
-          description="The white tiger, scientifically known as Panthera tigris,
-          is a majestic and rare subspecies of the Bengal tiger 
-          (Panthera tigris tigris). What sets the white tiger apart
-          from its orange counterparts is its unique genetic 
-          variation, resulting in a striking coat coloration characterized
-          by white fur with black stripes."
-        />
+        {animalCards}
       </main>
     </>
   )
---
+++ src/data/animalData.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        "key": 0,
+        "img": "/public/images/white-tiger.jpg",
+        "location": "India",
+        "locationLink": "https://goo.gl/maps/oPMqpb1hhGjdNapv7",
+        "title": "The White Tiger",
+        "altNames": "Ghost Tiger, Royal White Tiger",
+        "description": "The white tiger, scientifically known as Panthera tigris, is a majestic and rare subspecies of the Bengal tiger (Panthera tigris tigris). What sets the white tiger apart from its orange counterparts is its unique genetic variation, resulting in a striking coat coloration characterized by white fur with black stripes."
+    },
+    {
+        "key": 1,
+        "img": "/public/images/sumatran-elephant.jpg",
+        "location": "Sumatra, Indonesia",
+        "locationLink": "https://goo.gl/maps/tJxBjgYyvkJBm7NL8",
+        "title": "Sumatran Elephant",
+        "altNames": "Elephas maximus sumatranus",
+        "description": "The Sumatran elephant, scientifically known as Elephas maximus sumatranus, is one of the most endangered elephant subspecies. It is native to the island of Sumatra in Indonesia and faces severe threats due to habitat loss and poaching."
+    },
+    {
+        "key": 2,
+        "img": "/public/images/madagascar-lemur.jpg",
+        "location": "Madagascar",
+        "locationLink": "https://goo.gl/maps/wGxnf2QJbUc1diDg8",
+        "title": "Madagascar Lemur",
+        "altNames": "Various species",
+        "description": "Madagascar lemurs are a diverse group of primates endemic to the island of Madagascar. Many lemur species are critically endangered due to deforestation, habitat fragmentation, and illegal pet trade."
+    },
+    {
+        "key": 3,
+        "img": "/public/images/amur-leopard.jpg",
+        "location": "Amur River Basin, Russia and China",
+        "locationLink": "https://goo.gl/maps/tZrsRAZwBhfta2F86",
+        "title": "Amur Leopard",
+        "altNames": "Panthera pardus orientalis",
+        "description": "The Amur leopard, scientifically known as Panthera pardus orientalis, is one of the world's rarest big cats. It inhabits the Amur River Basin in Russia and China and is critically endangered, primarily due to habitat loss and poaching."
+    },
+    {
+        "key": 4,
+        "img": "/public/images/penguin.jpg",
+        "location": "Galápagos Islands, Ecuador",
+        "locationLink": "https://goo.gl/maps/S3W96kyKpt46Katb7",
+        "title": "Galápagos Penguin",
+        "altNames": "Spheniscus mendiculus",
+        "description": "The Galápagos penguin, scientifically known as Spheniscus mendiculus, is the only penguin species found north of the equator. It is native to the Galápagos Islands and is endangered due to threats such as climate change and human disturbance."
+    }
+]
